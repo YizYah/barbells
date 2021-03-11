8c38b961b2f1e544dd45568d69102a691b7be429
YizYah <yyakovson613@gmail.com>
2021-03-11 13:53:44 +0200

    feat(everything): updated template to work with CI

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 /* ns__file unit: standard, comp: src/index.ts */
 /* ns__custom_start beginning */
-const {loadFileTemplate} = require('./custom/loadFileTemplate')
-const {prepareHandlebars} = require('./custom/prepareHandlebars')
 /* ns__custom_end beginning */
 
 /* types */
 
     /* ns__custom_start export */
     // replace this with your own export
-    module.exports = {
-        loadFileTemplate,
-        prepareHandlebars,
-    }
+    module.exports = "hello world"
     /* ns__custom_end export */
 /* constants */
