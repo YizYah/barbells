14b44fdf0411b2310e9a2ca0eab159614565c258
YizYah <yyakovson613@gmail.com>
2021-03-15 14:24:44 +0200

    feat(index.js): added a temp to test coverage

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 /* ns__file unit: standard, comp: src/index.ts */
 /* ns__custom_start beginning */
+import {temp} from './custom/temp'
+
 const {loadFileTemplate} = require('./custom/loadFileTemplate')
 const {prepareHandlebars} = require('./custom/prepareHandlebars')
 /* ns__custom_end beginning */
 
 /* types */
-
+temp()
     /* ns__custom_start export */
 module.exports = {
     loadFileTemplate,
